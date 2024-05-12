62b38fd83407fb59ab8ba4edc140ed85fdd6c410
developerjay18 <jayrai3003@gmail.com>
2024-05-12 11:35:25 +0530

    making hero section changes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -10,19 +10,24 @@ import { AiOutlineWhatsApp } from 'react-icons/ai';
 
 function HeroSection() {
   return (
-    <div className="h-[45rem] md:h-[45rem] flex flex-col justify-center items-center px-5 md:px-20 bg-black" id='home'>
+    <div
+      className="h-[45rem] md:h-[45rem] flex flex-col justify-center items-center px-5 md:px-20 bg-black"
+      id="home"
+    >
+      {/* big screens  */}
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20 hidden md:block"
         fill="white"
       />
 
+      {/* small screens  */}
       <Spotlight
         className="-top-20 left-0 md:left-60 md:-top-20 md:hidden block"
         fill="white"
       />
 
       <div className="p-4 relative z-10 w-full text-center flex flex-col gap-3">
-        <h1 className="mt-20 md:mt-0 text-5xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 pb-8 leading-[3.2rem] max-w-5xl mx-auto">
+        <h1 className="mt-20 md:pt-28 md:mt-0 text-5xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 pb-8 leading-[3.2rem] max-w-5xl mx-auto">
           I Build Stunning Webistes for
           <span>
             <Typewriter
@@ -46,7 +51,7 @@ function HeroSection() {
         </h1>
 
         <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-xl mx-auto">
-          I&apos;m a skilled full stack web developer from Gujarat, India. I
+          I&apos;m a skilled full stack web developer from Ahmedabad, Gujarat, India. I
           specialize in crafting modern and visually striking websites to
           enhance businesse&apos;s online presence and unlock greater profit
           potential.
