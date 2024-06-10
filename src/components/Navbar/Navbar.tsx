b2863e82878f57611ae23a6993cfb2f55f051e0d
developerjay18 <jayrai3003@gmail.com>
2024-06-10 22:51:28 +0530

    updating navbar logo name

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -17,7 +17,7 @@ export default function Navbar({ className }: { className?: string }) {
           className="flex items-center space-x-3 rtl:space-x-reverse"
         >
           <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
-            JR.Web
+            JAY RAI
           </span>
         </Link>
         <button
